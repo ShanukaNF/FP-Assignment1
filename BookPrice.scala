60df4ec325ad3f12d73054af1fb6d9eddf6eff25
ShanukaNF <56572468+ShanukaNF@users.noreply.github.com>
2020-05-25 08:20:45 +0530

    1st Assignment

--- BookPrice.scala
+++ BookPrice.scala
@@ -1,7 +1,7 @@
 object BookTotal extends App{
 	def bookPrice(a:Int):Double = a * 24.95;
 	def discount(b:Double):Double = b * 0.4;
-	def shipping(x:Int):Double = 3 + (x - 50) * 0.75;//for the first 50 books shpping price is 3 Rupees
+	def shipping(x:Int):Double = if (x <= 50) 3 else 3 + (x - 50) * 0.75;//for the first 50 books shpping price is 3 Rupees
 	def BookTotal():Double =bookPrice(60) - discount(60) + shipping(60);
 	println(BookTotal);
 
